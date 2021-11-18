8c925bfbb73acf172de1425b713f9652bb519a5e
AlvaroJTorres <mangetrone@gmail.com>
2021-11-18 17:46:37 -0500

    Changed BASE_URL to a deployed one

--- assets/scripts/services/api_fetch.js
+++ assets/scripts/services/api_fetch.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://bsale-backend-test.herokuapp.com";
 
 async function apiFetch(...args) {
   const response = await fetch(...args);
