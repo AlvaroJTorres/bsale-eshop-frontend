8d62290c5e86bde7d9440fea14b5e2e1da213e1a
AlvaroJTorres <mangetrone@gmail.com>
2021-11-18 02:41:27 -0500

    Tested that when a query is placed in the loadSearchProducts method it returns the data from the API

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function init() {
-  console.log("Hello World");
+import { ProductsService } from "./assets/scripts/services/products_service.js";
+
+async function init() {
+  const productList = new ProductsService();
+  const data = await productList.loadSearchProducts("bebida energetica");
+  console.log(data);
 }
 init();
