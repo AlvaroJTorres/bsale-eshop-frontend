f47e8c6e0f24f7cf12051a63bc617e9fd329548e
AlvaroJTorres <mangetrone@gmail.com>
2021-11-23 21:59:01 -0500

    Updated sidebar so not it filter by category for the current products

--- assets/scripts/sidebar.js
+++ assets/scripts/sidebar.js
@@ -1,8 +1,8 @@
+import { generateProductCards } from "./helpers/generateProductsCards.js";
 import { CategoriesService } from "./services/categories_service.js";
-import { ProductsService } from "./services/products_service.js";
 import SidebarElement from "./sidebarElement.js";
-import ProductCard from "./productCard.js";
 import STORE from "./store.js";
+import { handleNotFound } from "./helpers/handleNotFound.js";
 
 export default function Sidebar() {
   this.parentElement = document.querySelector(".js-sidebar");
@@ -26,31 +26,21 @@ Sidebar.prototype.renderCategories = async function (parentSelector) {
   return categories;
 };
 
-Sidebar.prototype.generateProductCards = function (parentSelector) {
-  const container = document.querySelector(parentSelector);
-  const productCards = STORE.products.map((productData) => {
-    return new ProductCard(productData);
-  });
-  container.innerHTML = productCards.join("");
-  return productCards;
-};
-
 Sidebar.prototype.handleClick = function () {
   const categoryList = document.querySelector(".js-list");
 
   categoryList.addEventListener("click", async (e) => {
     if (e.target.id == "js-category") {
       e.preventDefault();
-      console.log(e.target.innerText);
-      try {
-        const productsService = new ProductsService();
-        const query = e.target.innerText;
-        const productsList = await productsService.loadSearchProducts(query);
-        STORE.products = productsList;
-        this.generateProductCards(".js-products_list");
-      } catch (e) {
-        alert(e.message);
-      }
+
+      const category = +e.target.dataset.id;
+      const filteredProductsList = STORE.products.filter(
+        (product) => product.category == category
+      );
+      STORE.products = filteredProductsList;
+      STORE.products.length > 0
+        ? generateProductCards(".js-products_list")
+        : handleNotFound(".js-products_list");
     }
   });
 };
