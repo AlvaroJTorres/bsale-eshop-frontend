77d70b063ebeb616a9508059373bacfc870bba17
AlvaroJTorres <mangetrone@gmail.com>
2021-11-23 20:56:33 -0500

    Updated error message when a product is not found

--- assets/scripts/errorMessage.js
+++ assets/scripts/errorMessage.js
@@ -2,7 +2,7 @@ export default function ErrorMessage() {
   this.toString = function () {
     return `
     <div class="container">
-      <p>Esa categoría no se encuentra disponible, favor de ingresar una de la lista</p>
+      <p>No existe un producto con ese término</p>
       <p>Gracias!</p>         
     </div>
     `;
