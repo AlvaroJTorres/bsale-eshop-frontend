0c8a89e810dc1c92cad44e0b10c5a767b1b31b2a
AlvaroJTorres <mangetrone@gmail.com>
2021-11-23 22:00:17 -0500

    Updated init function so now it renders all the Products in the first page load

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+import { generateProductCards } from "./assets/scripts/helpers/generateProductsCards.js";
 import SearchBar from "./assets/scripts/searchBar.js";
+import { ProductsService } from "./assets/scripts/services/products_service.js";
 import Sidebar from "./assets/scripts/sidebar.js";
+import STORE from "./assets/scripts/store.js";
 
 async function init() {
   const searchBar = new SearchBar();
   const sidebar = new Sidebar();
+  const productService = new ProductsService();
+  const productsList = await productService.loadProducts();
+  STORE.products = productsList;
 
   searchBar.render();
+  generateProductCards(".js-products_list");
   sidebar.render();
 }
 
